f241ff386d721bd3c6507333e96b6ee5ebca7bea
keshav9926 <kkakani160@gmail.com>
2026-06-07 01:15:38 +0530

    react_app_url

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
+
 const api = axios.create({
-  baseURL: `${process.env.BACKEND_BASE_URL}/api/v1`,
+  baseURL: `${process.env.REACT_APP_BACKEND_BASE_URL}/api/v1`,
   withCredentials: true,
 });
-
-export default api;
+console.log("BACKEND:", process.env.REACT_APP_BACKEND_BASE_URL);
+console.log("REACT:", process.env.REACT_APP_BACKEND_BASE_URL);
+export default api; 
